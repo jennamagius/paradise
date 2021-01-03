861570966cc04eb6b1b0b0e1b23e3b4258d5e8e7
Jenna Magius <jenna@jenna.app>
2021-01-03 00:54:32 -0700

    Early development

--- engine/src/config.rs
+++ engine/src/config.rs
@@ -3,4 +3,7 @@ pub struct Config {
     /// An sqlx connection string indicating how to connect to the database
     #[structopt(long, env = "PARADISE_DB", hide_env_values = true)]
     pub db: String,
+    /// Addresses to accept incoming connections on
+    #[structopt(long)]
+    pub bind: Vec<::std::net::SocketAddr>,
 }
--- engine/src/paradise.rs
+++ engine/src/paradise.rs
@@ -5,6 +5,11 @@ pub struct Paradise {
 struct Internal {
     config: crate::config::Config,
     db_pool: ::sqlx::Pool<::sqlx::Postgres>,
+    sq: ::parking_lot::Mutex<::iou::SubmissionQueue<'static>>,
+    cq: ::parking_lot::Mutex<::iou::CompletionQueue<'static>>,
+    registrar: ::iou::Registrar<'static>,
+    binds: ::parking_lot::Mutex<Vec<::std::os::unix::io::RawFd>>,
+    done: ::std::sync::atomic::AtomicBool,
 }
 
 impl Paradise {
@@ -19,13 +24,73 @@ impl Paradise {
                 return Err(anyhow::Error::new(err));
             }
         };
-        let i = Internal { config, db_pool };
+        let uring = Box::new(::iou::IoUring::new(16)?);
+        let uring = Box::leak(uring);
+        let (sq, cq, registrar) = uring.queues();
+        let sq = ::parking_lot::Mutex::new(sq);
+        let cq = ::parking_lot::Mutex::new(cq);
+        let done = false.into();
+        let i = Internal {
+            config,
+            db_pool,
+            sq,
+            cq,
+            registrar,
+            done,
+            binds: Default::default(),
+        };
         let i = ::std::sync::Arc::new(i);
         let result = Paradise { i };
+        result.init_binds()?;
         Ok(result)
     }
 
+    fn init_binds(&self) -> ::anyhow::Result<()> {
+        for addr in &self.i.config.bind {
+            let af = match addr {
+                ::std::net::SocketAddr::V4(_) => ::nix::sys::socket::AddressFamily::Inet,
+                ::std::net::SocketAddr::V6(_) => ::nix::sys::socket::AddressFamily::Inet6,
+            };
+            let socket = ::nix::sys::socket::socket(
+                af,
+                ::nix::sys::socket::SockType::Stream,
+                ::nix::sys::socket::SockFlag::SOCK_CLOEXEC,
+                ::nix::sys::socket::SockProtocol::Tcp,
+            )?;
+            let addr = ::nix::sys::socket::InetAddr::from_std(&addr);
+            let addr = ::nix::sys::socket::SockAddr::Inet(addr);
+            ::nix::sys::socket::bind(socket, &addr)?;
+            ::nix::sys::socket::listen(socket, 5)?;
+            let mut sq = self.i.sq.lock();
+            let mut event = match sq.prepare_sqe() {
+                Some(x) => x,
+                None => ::anyhow::bail!("Out of submission queue events"),
+            };
+            unsafe { event.prep_accept(socket, None, ::iou::sqe::SockFlag::SOCK_CLOEXEC) };
+            self.i.binds.lock().push(socket);
+        }
+        self.i.sq.lock().submit();
+        log::trace!("Binds finished");
+        Ok(())
+    }
+
     pub async fn run(&self) -> anyhow::Result<()> {
+        ::tokio::task::block_in_place(|| {
+            let mut cq = self.i.cq.lock();
+            loop {
+                log::trace!("Waiting for event.");
+                let event = cq.wait_for_cqe();
+                log::trace!("Got an event.");
+                let event = match event {
+                    Ok(x) => x,
+                    Err(err) => {
+                        log::warn!("Event error: {}.", err);
+                        continue;
+                    }
+                };
+                log::trace!("Event user data: {}.", event.user_data());
+            }
+        });
         Ok(())
     }
 }
